9ed55f64a01cbc0119a2e17ef62433a9f94321be
Marcos Lilljedahl <marcosnils@gmail.com>
2019-11-05 02:28:21 +0000

    Add path

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_lambda_function" "test_lambda" {
   role          = "${aws_iam_role.iam_for_lambda.arn}"
   handler       = "index.test"
 
-  source_code_hash = "${filebase64sha256("lambda_function_payload.zip")}"
+  source_code_hash = "${filebase64sha256("${path.module}/lambda_function_payload.zip")}"
 
   runtime = "nodejs10.x"
 
@@ -37,11 +37,11 @@ resource "aws_lambda_function" "test_lambda" {
 
 resource "null_resource" "deps_validation" {
   triggers = {
-    authorizer_hash = "${filebase64sha256("lambda_function_payload.zip")}"
+    authorizer_hash = "${filebase64sha256("${path.module}/lambda_function_payload.zip")}"
   }
 
   provisioner "local-exec" {
     # Checks that node_modules is inside zip file
-    command = "unzip -l lambda_function_payload.zip | grep figlet > /dev/null"
+    command = "unzip -l ${path.module}/lambda_function_payload.zip | grep figlet > /dev/null"
   }
 }
